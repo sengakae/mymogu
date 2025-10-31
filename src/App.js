d19072ba4ba5300913515eaf4ca8b4de6a59e4a4
sengakae <esa42@naver.com>
2025-10-31 10:01:32 -0700

    add speedinsights

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Password from "./components/Password";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <Router>
+      <SpeedInsights />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/password" element={<Password />} />
